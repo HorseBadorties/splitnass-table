a6fe535e6329a840b4e2bef6b1c5cb2097c45cf0
HorseBadorties <no@thanks.com>
2018-07-12 17:01:16 +0200

    clean-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,19 @@ export interface Runde {
 }
 
 const RUNDEN: Runde[] = [
-  {nr: 14, torsten: '*', guido: '*', claus: '*', thomas: '-', levent: '*', boecke: '|||', punkte: 16},
-  {nr: 15, torsten: '*', guido: '156', claus: '*', thomas: '120', levent: '-', boecke: '||', punkte: 32},
-  {nr: 16, torsten: '-', guido: '168', claus: '*', thomas: '132', levent: '*', boecke: '||', punkte: 12},
-  {nr: 17, torsten: '*', guido: '-', claus: '141', thomas: '144', levent: '*', boecke: '||', punkte: 12},
-  {nr: 18, torsten: '*', guido: '174', claus: '-', thomas: '*', levent: '*', boecke: '|', punkte: 6},
-  {nr: 19, torsten: '*', guido: '178', claus: '-', thomas: '*', levent: '109', boecke: '|', punkte: 4},
-  {nr: 20, torsten: '62', guido: '*', claus: '149', thomas: '-', levent: '*', boecke: '|', punkte: 8},
-  {nr: 21, torsten: '*', guido: '*', claus: '161', thomas: '156', levent: '-', boecke: '|', punkte: 12},
-  {nr: 22, torsten: '-', guido: '*', claus: '*', thomas: '168', levent: '121', boecke: '|', punkte: 12},
-  {nr: 23, torsten: '68', guido: '-', claus: '167', thomas: '*', levent: '*', boecke: '|', punkte: 6},
-  {nr: 24, torsten: '*', guido: '180', claus: '-', thomas: '170', levent: '*', boecke: '', punkte: 2},
-  {nr: 25, torsten: '*', guido: '198', claus: '185', thomas: '-', levent: '*', boecke: '|', punkte: 18},
+  {nr: 14, torsten: '*',  guido: '*',   claus: '*',   thomas: '-',    levent: '*',    boecke: '|||',  punkte: 16},
+  {nr: 15, torsten: '*',  guido: '156', claus: '*',   thomas: '120',  levent: '-',    boecke: '||',   punkte: 32},
+  {nr: 16, torsten: '-',  guido: '168', claus: '*',   thomas: '132',  levent: '*',    boecke: '||',   punkte: 12},
+  {nr: 17, torsten: '*',  guido: '-',   claus: '141', thomas: '144',  levent: '*',    boecke: '||',   punkte: 12},
+  {nr: 18, torsten: '*',  guido: '174', claus: '-',   thomas: '*',    levent: '*',    boecke: '|',    punkte: 6},
+  {nr: 19, torsten: '*',  guido: '178', claus: '-',   thomas: '*',    levent: '109',  boecke: '|',    punkte: 4},
+  {nr: 20, torsten: '62', guido: '*',   claus: '149', thomas: '-',    levent: '*',    boecke: '|',    punkte: 8},
+  {nr: 21, torsten: '*',  guido: '*',   claus: '161', thomas: '156',  levent: '-',    boecke: '|',    punkte: 12},
+  {nr: 22, torsten: '-',  guido: '*',   claus: '*',   thomas: '168',  levent: '121',  boecke: '|',    punkte: 12},
+  {nr: 23, torsten: '68', guido: '-',   claus: '167', thomas: '*',    levent: '*',    boecke: '|',    punkte: 6},
+  {nr: 24, torsten: '*',  guido: '180', claus: '-',   thomas: '170',  levent: '*',    boecke: '',     punkte: 2},
+  {nr: 25, torsten: '*',  guido: '198', claus: '185', thomas: '-',    levent: '*',    boecke: '|',    punkte: 18},
 ];
-
 
 @Component({
   selector: 'app-root',
@@ -50,7 +49,7 @@ export class AppComponent  {
   }
 
   rundeClicked(runde: Runde) {
-    console.log(`Runde ${runde.nr} clicked`)
+    console.log(`Runde ${runde.nr} clicked`);
     this.focusRunde(runde);
   }
 
@@ -58,25 +57,20 @@ export class AppComponent  {
     this.focused = runde;
   }
   
-  recTrackBy(index: number, runde: Runde){
-    return runde.nr;
-  }
   scrollToRunde(runde: Runde) {
-    console.log("scrolling to " + runde.nr);
-    this.showElement(runde.nr, 20);
-    this.focusRunde(runde);
+    if (runde != null) {      
+      this.scrollToNr(runde.nr.toString());
+      this.focusRunde(runde);
+    }
   }
 
-  showElement(index: number, height: number) {    
-    let indexstr = index.toString();
-    let row = this.rows.find(row => row.element.nativeElement.getAttribute('nr') === indexstr); 
+  scrollToNr(nr: string) {    
+    let row = this.rows.find(row => row.element.nativeElement.getAttribute('nr') === nr); //[attr.nr]="getNrByRunde(runde)"
     if (row != null) {
-      let rect = row.element.nativeElement.getBoundingClientRect();
-      if ((rect.y <= 0) || ((rect.y+rect.height) > height)) {        
         row.element.nativeElement.scrollIntoView(false, {behavior: 'smooth'});
-      }
+        console.log("scrolled to " + nr);
     } else {
-      console.log('not found');
+      console.log("failed to scroll to " + nr);
     }
   }
 }
