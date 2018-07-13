e70b1603b5ecd7066f5e13d808dbb69b157e370a
HorseBadorties <HorseBadorties@whereever.com>
2018-07-13 23:12:24 +0200

    fiddling

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/splitnass-table/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default
