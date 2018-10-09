573dde7137bd35095b80066c4da5cd919dc6804a
HorseBadorties <HorseBadorties@whereever.com>
2018-10-09 10:19:26 +0200

    test auto-deploy to heroku

--- src/app/model/Runde.ts
+++ src/app/model/Runde.ts
@@ -1,15 +1,15 @@
 export class Runde {
     nr: number;
-    torsten: string = "";  
-    guido: string = "";  
-    claus: string = "";  
-    thomas: string = "";  
-    levent: string = "";  
-    boecke: number;
-    punkte: number;
+    torsten = '';
+    guido = '';
+    claus = '';
+    thomas = '';
+    levent = '';
+    boecke = '';
+    punkte = '';
 
     constructor(nr: number) {
       this.nr = nr;
     }
-        
+
   }
