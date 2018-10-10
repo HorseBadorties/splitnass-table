caf95b5ef03b6c0ee5afb5aa7ac19639f3f8c621
HorseBadorties <no@thanks.com>
2018-10-10 14:04:30 +0200

    material removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import {Component, ViewChildren, QueryList, ViewContainerRef, ElementRef, OnInit} from '@angular/core';
+import {Component, ViewChildren, QueryList, ElementRef, OnInit} from '@angular/core';
 
 import { Runde } from './model/Runde';
 import { RundenService } from './runden.service';
-import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -14,16 +13,13 @@ export class AppComponent implements OnInit {
   selectedRunde: Runde;
   displayedColumns: String[];
   runden: Runde[];
-  dataSource: MatTableDataSource<Runde>;
   // https://netbasal.com/understanding-viewchildren-contentchildren-and-querylist-in-angular-896b0c689f6e
-  @ViewChildren('matrow', { read: ElementRef }) rows: QueryList<ElementRef>;
   @ViewChildren('primerow', { read: ElementRef }) rowsPrime: QueryList<ElementRef>;
 
   constructor(public rundenService: RundenService) {}
 
   ngOnInit() {
     this.rundenService.getRunden().subscribe(_runden => {
-      this.dataSource = new MatTableDataSource(_runden);
       this.runden = _runden;
     });
     this.rundenService.getColumnNames().subscribe(columns => this.displayedColumns = columns);
@@ -32,7 +28,7 @@ export class AppComponent implements OnInit {
   addRunde() {
     let newRunde: Runde;
     this.rundenService.addRunde().subscribe(r => newRunde = r);
-    this.dataSource.data = this.dataSource.data.concat(newRunde);  // re-assigning data triggers the mat-table update
+    this.runden = this.runden.concat(newRunde);  // re-assigning data triggers the mat-table update
     this.scrollToRunde(this.getRundeByNr(newRunde.nr - 1));
   }
 
@@ -42,7 +38,7 @@ export class AppComponent implements OnInit {
 
   getRundeByNr(nr: number): Runde {
     console.log('getRundeByNr ' + nr);
-    return this.dataSource.data.find(r => r.nr == nr);
+    return this.runden.find(r => r.nr == nr);
   }
 
   rundeClicked(runde: Runde) {
@@ -62,7 +58,6 @@ export class AppComponent implements OnInit {
   }
 
   scrollToNr(nr: string) {
-    this.scrollTo(this.rows.find(r => r.nativeElement.getAttribute('nr') === nr));
     this.scrollTo(this.rowsPrime.find(r => r.nativeElement.getAttribute('nr') === nr));
   }
 
