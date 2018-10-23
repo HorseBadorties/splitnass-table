734e5c6fe76527b75421a136e33ec774c3f135b0
HorseBadorties <HorseBadorties@whereever.com>
2018-10-23 11:54:04 +0200

    rename Runde

--- src/app/model/spieler.ts
+++ src/app/model/spieler.ts
@@ -1,21 +1,10 @@
-export interface ISpieler {
-    id: number;
-    name: string;
-}
 
-export class Spieler implements ISpieler {
+export class Spieler  {
     constructor(public id: number, public name: string) {}
 
     toJSON() {
         return this.id;
     }
 
-    static fromJSON(json: string): ISpieler {
-      return JSON.parse(json, (key, value) => {      
-        let result = new Spieler(value, null);
-        result.name = 'restored';
-        return result;
-      });
-    }
 }
 
